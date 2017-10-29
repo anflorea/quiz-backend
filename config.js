67b8a47b35e660086b9386a6236291d5943faf64
Andrei Florea <floreaandreimihai@gmail.com>
2017-10-29 19:39:02 +0200

    Fixes database url for production

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ const dbName = 'whateverdb';
 
 export default {
   'secret': 'whateversecret',
-  'database': `mongodb://127.0.0.1:27017/${dbName}`
+  'database': `mongodb://127.0.0.1:27017/${dbName}`,
+  'production-database': proccess.env.MONGODB_URI
 }
