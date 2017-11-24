dc3b3b2571af5bce0383b009d243a6eb3367547f
Andrei Florea <floreaandreimihai@gmail.com>
2017-11-24 11:17:48 +0200

    Added update and delete user

--- app/controllers/api/users.js
+++ app/controllers/api/users.js
@@ -33,4 +33,31 @@ router.post('/', (req, res) => {
   });
 });
 
+router.put('/:id', (req, res) => {
+  User.findById(req.params.id, function (err, user) {
+    if (err) throw err;
+    
+    if (req.body.email)
+      user.email = req.body.email;
+    if (req.body.firstName)
+      user.firstName = req.body.firstName;
+    if (req.body.lastName)
+      user.lastName = req.body.lastName;
+
+    user.save(function (err, updatedUser) {
+      if (err) throw err;
+      res.send({message: 'User updated successfully.'});
+    });
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  User.findById(req.params.id, function (err, user) {
+    if (err)
+      res.status(404).json({message: 'User not found.'});
+  });
+  User.findByIdAndRemove(req.params.id).exec();
+  res.send({message: "Technology deleted successfully"});
+});
+
 export default router;
