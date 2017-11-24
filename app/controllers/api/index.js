109246aafd0e68da1ce35a91800151865c16259a
Andrei Florea <floreaandreimihai@gmail.com>
2017-11-24 13:43:40 +0200

    Adds crud on users (#19)
    * Adds users controller and route for get user by id
    
    * Fixes user typo in index.js
    
    * Added update and delete user
    
    * Fixes user deletion confirmation message

--- app/controllers/api/index.js
+++ app/controllers/api/index.js
@@ -1,34 +1,17 @@
 import { Router } from 'express';
 import User from '../../models/user';
 import technologies from './technologies';
+import users from './users';
 import difficulties from './difficulties';
+
 const router = Router();
 
 router.get('/', (req, res) => {
   res.send(`Hello! Running...`);
 });
 
-router.get('/users', (req, res) => {
-  User.find({}, (err, users) => {
-    res.json(users);
-  });
-});
-router.post('/users', (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName
-  });
 
-  newUser.save((err) => {
-    if (err) throw err;
-
-    res.json({ message: "Sign up successful!" });
-  });
-});
-
+router.use('/users', users);
 router.use('/technologies', technologies);
 router.use('/difficulties', difficulties);
 
---
+++ app/controllers/api/users.js
@@ -0,0 +1,63 @@
+import { Router } from 'express';
+import User from '../../models/user';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  User.find({}, (err, users) => {
+    res.json(users);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  User.findById(req.params.id, function(err, user) {
+    if (err) throw err;
+
+    res.json(user);
+  });
+});
+
+router.post('/', (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName
+  });
+
+  newUser.save((err) => {
+    if (err) throw err;
+
+    res.json({ message: "Sign up successful!" });
+  });
+});
+
+router.put('/:id', (req, res) => {
+  User.findById(req.params.id, function (err, user) {
+    if (err) throw err;
+    
+    if (req.body.email)
+      user.email = req.body.email;
+    if (req.body.firstName)
+      user.firstName = req.body.firstName;
+    if (req.body.lastName)
+      user.lastName = req.body.lastName;
+
+    user.save(function (err, updatedUser) {
+      if (err) throw err;
+      res.send({message: 'User updated successfully.'});
+    });
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  User.findById(req.params.id, function (err, user) {
+    if (err)
+      res.status(404).json({message: 'User not found.'});
+  });
+  User.findByIdAndRemove(req.params.id).exec();
+  res.send({message: "User deleted successfully"});
+});
+
+export default router;
