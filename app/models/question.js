77ca547e7bdc817f5f5075636393c4ad15d6baf0
Emanuel Truța <lawleagle@gmail.com>
2017-12-15 01:56:01 +0200

    Questions and Types CRUD. (#21)

--- app/models/question.js
+++ app/models/question.js
@@ -3,4 +3,11 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 export default mongoose.model('Question', new Schema({
+    requirements: String,
+    rightAnswers: [String],
+    wrongAnswers: [String],
+    timeToAnswer: Number,
+    technology: {type: Schema.Types.ObjectId, ref: 'Technology'},
+    difficultyLevel: {type: Schema.Types.ObjectId, ref: 'DifficultyLevel'},
+    type: {type: Schema.Types.ObjectId, ref: 'Type'}
 }));
